0cff5a54e2d2740f1d55c848361654c808bb76b8
Paulien <paulien.m.73@gmail.com>
2020-02-12 14:34:40 +0100

    Authentication

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function testLogin()
+    {
+        $client = self::createClient();
+        $client->request('GET', '/login');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testLogout()
+    {
+        $client = self::createClient();
+        $client->followRedirects();
+        $client->request('GET', '/logout');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
