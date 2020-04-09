c8561bed75853f8a60a3ebb6f4b94fe11573762f
paulien MARANGE <paulien.marange@theia.fr>
2020-04-09 23:02:50 +0200

    Test redirect to homepage if logged user try to access to register route

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -4,6 +4,7 @@
 namespace App\Tests\Controller;
 
 
+use App\Tests\TestHelper;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RegistrationControllerTest extends WebTestCase
@@ -13,5 +14,18 @@ class RegistrationControllerTest extends WebTestCase
         $client = self::createClient();
         $client->request('GET', '/register');
         self::assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testRedirectLoggedUserToHomepage()
+    {
+        $client = self::createClient(
+            [],
+            [
+                'PHP_AUTH_USER' => 'username',
+                'PHP_AUTH_PW'   => 'password',
+            ]
+        );
+        $client->request('GET', '/register');
+        self::assertResponseRedirects('/');
     }
 }
